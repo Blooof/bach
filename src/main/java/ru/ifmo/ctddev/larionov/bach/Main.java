15ce2be460a7cd2e7654f2a5449922ea73489119
Oleg Larionov <blloof@gmail.com>
2013-05-02 23:16:42 +0400

    initial commit with pom

---
+++ src/main/java/ru/ifmo/ctddev/larionov/bach/Main.java
@@ -0,0 +1,12 @@
+package ru.ifmo.ctddev.larionov.bach;
+
+/**
+ * User: Oleg Larionov
+ * Date: 02.05.13
+ * Time: 23:13
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
