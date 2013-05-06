61e7d6269198bb43e7dfb7545d4b476d213d8432
Oleg Larionov <blloof@gmail.com>
2013-05-07 00:08:54 +0400

    URLComparator

---
+++ Classifier/src/main/java/ru/ifmo/ctddev/larionov/bach/comparator/URLComparator.java
@@ -0,0 +1,34 @@
+package ru.ifmo.ctddev.larionov.bach.comparator;
+
+import ru.ifmo.ctddev.larionov.bach.site.Site;
+
+import java.util.StringTokenizer;
+
+import static java.lang.Math.max;
+
+/**
+ * User: Oleg Larionov
+ * Date: 06.05.13
+ * Time: 23:33
+ */
+public class URLComparator implements Comparator {
+
+    @Override
+    public double compare(Site first, Site second) {
+        StringTokenizer firstTokenizer = new StringTokenizer(first.getUrl(), "/");
+        StringTokenizer secondTokenizer = new StringTokenizer(second.getUrl(), "/");
+
+        int firstTokensCount = firstTokenizer.countTokens();
+        int secondTokensCount = secondTokenizer.countTokens();
+        int similarTokensCount = 0;
+        while (firstTokenizer.hasMoreTokens() && secondTokenizer.hasMoreTokens()) {
+            String firstCurrentToken = firstTokenizer.nextToken();
+            String secondCurrentToken = secondTokenizer.nextToken();
+            if (firstCurrentToken.equals(secondCurrentToken)) {
+                ++similarTokensCount;
+            }
+        }
+
+        return ((double) similarTokensCount) / max(firstTokensCount, secondTokensCount);
+    }
+}
---
+++ Classifier/src/test/java/ru/ifmo/ctddev/larionov/bach/comparator/URLComparatorTest.java
@@ -0,0 +1,52 @@
+package ru.ifmo.ctddev.larionov.bach.comparator;
+
+import org.junit.Test;
+import ru.ifmo.ctddev.larionov.bach.site.Site;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * User: Oleg Larionov
+ * Date: 06.05.13
+ * Time: 23:42
+ */
+public class URLComparatorTest {
+
+    private static final double EPS = 1e-6;
+    private static final URLComparator urlComparator = new URLComparator();
+
+    @Test
+    public void testCompareIdenticalURLs() {
+        String url = "http://some.site/with/long/url";
+        Site first = getSiteWithUrl(url);
+        Site second = getSiteWithUrl(url);
+
+        double actual = urlComparator.compare(first, second);
+
+        assertEquals(1.0f, actual, EPS);
+    }
+
+    @Test
+    public void testCompareFullyDifferentURLs() {
+        Site first = getSiteWithUrl("https://fully/different/urls");
+        Site second = getSiteWithUrl("http://not/a/single/match");
+
+        double actual = urlComparator.compare(first, second);
+
+        assertEquals(0.0f, actual, EPS);
+    }
+
+    @Test
+    public void testFiftyPercentSimilar() {
+        Site first = getSiteWithUrl("http://some/fifty/percent/similar/urls");
+        Site second = getSiteWithUrl("http://exactly/50/percent/similar/sites");
+
+        double actual = urlComparator.compare(first, second);
+
+        assertEquals(0.5f, actual, EPS);
+    }
+
+    private Site getSiteWithUrl(String url) {
+        return new Site(url);
+    }
+}
